d6d20d0d757ddc75626226719ea46215777bc13b
IT21160370 <99235785+IT21160370@users.noreply.github.com>
2022-05-09 11:15:51 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "order.h"
+
+using namespace std;
+
+int main() {
+  order orderNo,orderDate;
+  orderNo
+  orderDate
+}
---
+++ order.h
@@ -0,0 +1,9 @@
+class order{
+private:
+    char orderNO[];
+    char orderDate[]; 
+
+public:
+    void dispalyOrderDetais(char orderNO,char orderDate);
+
+};
