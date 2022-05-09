babe988856c7538ff55c3d6d9afd69eb4889b660
IT21160370 <99235785+IT21160370@users.noreply.github.com>
2022-05-09 07:59:07 +0000

    create order class.

---
+++ Order.cpp
@@ -0,0 +1,20 @@
+class Order{
+private:
+    char orderNO[10];
+    char orderDate[10]; 
+
+public: 
+    Order(){
+      strcpy(orderNo, "");
+      strcpy(orderDate, "");
+    }
+    Order(char Onumber[], char Odate[]){
+        strcpy(orderNo, Onumber);
+        strcpy(orderDate, Odate);
+    }
+    void dispalyOrderDetais();{
+        cout << "Order No : " << orderNo << endl;
+        cout << "Order Date : " << orderDate << endl;
+    }
+
+};
--- order.h
+++
@@ -1,9 +0,0 @@
-class order{
-private:
-    char orderNO[];
-    char orderDate[]; 
-
-public:
-    void dispalyOrderDetais(char orderNO,char orderDate);
-
-};
